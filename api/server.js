83a64eb564864da4f8ff611e886a1912e45c118e
hans0 <kiessler.hans@gmail.com>
2021-05-03 17:40:50 -0400

    Done with MVP

--- api/server.js
+++ api/server.js
@@ -56,10 +56,49 @@ server.get('/api/users/:id', async (req, res) => {
 });
 
 // DELETE 	/api/users/:id 	Removes the user with the specified id and returns the deleted user.
-// server.delete('/api/users')
+server.delete('/api/users/:id', (req, res) => {
+  Users.delete(req.params.id)
+    .then(deletedUser => {
+      if (!deletedUser){
+        res.status(404).json({
+          message: "The user with the specified ID does not exist"
+        });
+      } else {
+        res.json(deletedUser);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "The user could not be removed"
+      });
+    })
+});
 
 // PUT 	/api/users/:id 	Updates the user with the specified id using data from the request body. Returns the modified user
-
+server.put('/api/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, bio } = req.body;
+    if (!name || !bio) {
+      res.status(400).json({
+        message: "Please provide name and bio for the user",
+      });
+    } else {
+      const updatedUser = await Users.update(id, { name, bio });
+      if (!updatedUser) {
+        res.status(404).json({
+          message: "The user with the specified ID does not exist",
+        });
+      } else {
+        res.status(200).json(updatedUser);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The user information could not be modified",
+    });
+  }
+})
 
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
